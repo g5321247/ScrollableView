13e51a8be64a654ea2e5d8824a2d6d3d92cfc0d3
GeorgeLiu <george_liu@addcn.com>
2020-07-23 15:46:37 +0800

    Initial Commit

---
+++ ScrollableCardView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollableCardView
+//
+//  Created by 劉峻岫 on 2020/7/23.
+//  Copyright © 2020 addcn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
